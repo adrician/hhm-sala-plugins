7e5597e11d1790475531d9b8572352308588afd0
adrician <52471788+adrician@users.noreply.github.com>
2020-12-24 18:16:25 +0200

    Update simple-commands.js

--- src/hr/simple-commands.js
+++ src/hr/simple-commands.js
@@ -3,7 +3,7 @@
  * with a public room.
  *
  * Some code copied from XHerna's plugin.
- * See original at https://github.com/XHerna/hhm-plugins/blob/master/src/tut/simple-commands.js.
+ * See original at https://github.com/XHerna/hhm-plugins/blob/master/hr/tut/simple-commands.js.
  */
 
 const room = HBInit();
@@ -12,11 +12,162 @@ function isGameRunning() {
   return !!room.getScores();
 }
 
+var strongClassic = {
+
+	"name" : "Strongball Classic",
+
+	"width" : 420,
+
+	"height" : 200,
+
+	"spawnDistance" : 170,
+
+	"bg" : { "type" : "grass", "width" : 370, "height" : 170, "kickOffRadius" : 75, "cornerRadius" : 0 },
+
+	"vertexes" : [
+		/* 0 */ { "x" : -370, "y" : 170, "trait" : "ballArea" },
+		/* 1 */ { "x" : -370, "y" : 64, "trait" : "ballArea" },
+		/* 2 */ { "x" : -370, "y" : -64, "trait" : "ballArea" },
+		/* 3 */ { "x" : -370, "y" : -170, "trait" : "ballArea" },
+		/* 4 */ { "x" : 370, "y" : 170, "trait" : "ballArea" },
+		/* 5 */ { "x" : 370, "y" : 64, "trait" : "ballArea" },
+		/* 6 */ { "x" : 370, "y" : -64, "trait" : "ballArea" },
+		/* 7 */ { "x" : 370, "y" : -170, "trait" : "ballArea" },
+
+		/* 8 */ { "x" : 0, "y" : 200, "trait" : "kickOffBarrier" },
+		/* 9 */ { "x" : 0, "y" : 75, "trait" : "kickOffBarrier" },
+		/* 10 */ { "x" : 0, "y" : -75, "trait" : "kickOffBarrier" },
+		/* 11 */ { "x" : 0, "y" : -200, "trait" : "kickOffBarrier" },
+
+		/* 12 */ { "x" : -380, "y" : -64, "trait" : "goalNet" },
+		/* 13 */ { "x" : -400, "y" : -44, "trait" : "goalNet" },
+		/* 14 */ { "x" : -400, "y" : 44, "trait" : "goalNet" },
+		/* 15 */ { "x" : -380, "y" : 64, "trait" : "goalNet" },
+		/* 16 */ { "x" : 380, "y" : -64, "trait" : "goalNet" },
+		/* 17 */ { "x" : 400, "y" : -44, "trait" : "goalNet" },
+		/* 18 */ { "x" : 400, "y" : 44, "trait" : "goalNet" },
+		/* 19 */ { "x" : 380, "y" : 64, "trait" : "goalNet" },
+
+		/* 20 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -45.9999980927, "y" : 175.714294434 },
+		/* 21 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -45.9999980927, "y" : 196.714294434 },
+		/* 22 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -36.9999980927, "y" : 196.714294434 },
+		/* 23 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -34.9999980927, "y" : 176.714294434 },
+		/* 24 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -18.9999980927, "y" : 177.714294434 },
+		/* 25 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -18.9999980927, "y" : 196.714294434 },
+		/* 26 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -4.99999809265, "y" : 176.714294434 },
+		/* 27 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -3.99999809265, "y" : 198.714294434 },
+		/* 28 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -3.99999809265, "y" : 186.714294434 },
+		/* 29 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : 34.0000019073, "y" : 176.714294434 },
+		/* 30 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : 33.0000019073, "y" : 198.714294434 },
+		/* 31 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : 48.0000019073, "y" : 174.714294434 },
+		/* 32 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : 55.0000019073, "y" : 186.714294434 },
+		/* 33 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : 61.0000019073, "y" : 175.714294434 },
+		/* 34 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : 54.0000019073, "y" : 198.714294434 },
+		/* 35 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -18.9999980927, "y" : 177.714294434, "curve" : 173.974424992 },
+		/* 36 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : -18.9999980927, "y" : 196.714294434, "curve" : 173.974424992 },
+		/* 37 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : 18.0000019073, "y" : 177.714294434 },
+		/* 38 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : 18.0000019073, "y" : 196.714294434 },
+		/* 39 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : 18.0000019073, "y" : 177.714294434, "curve" : 173.974424992 },
+		/* 40 */ { "cMask" : ["ball" ], "cGroup" : ["ball" ], "x" : 18.0000019073, "y" : 196.714294434, "curve" : 173.974424992 }
+
+	],
+
+	"segments" : [
+		{ "v0" : 0, "v1" : 1, "trait" : "ballArea" },
+		{ "v0" : 2, "v1" : 3, "trait" : "ballArea" },
+		{ "v0" : 4, "v1" : 5, "trait" : "ballArea" },
+		{ "v0" : 6, "v1" : 7, "trait" : "ballArea" },
+
+		{ "v0" : 12, "v1" : 13, "trait" : "goalNet", "curve" : -90 },
+		{ "v0" : 13, "v1" : 14, "trait" : "goalNet" },
+		{ "v0" : 14, "v1" : 15, "trait" : "goalNet", "curve" : -90 },
+		{ "v0" : 16, "v1" : 17, "trait" : "goalNet", "curve" : 90 },
+		{ "v0" : 17, "v1" : 18, "trait" : "goalNet" },
+		{ "v0" : 18, "v1" : 19, "trait" : "goalNet", "curve" : 90 },
+
+		{ "v0" : 8, "v1" : 9, "trait" : "kickOffBarrier" },
+		{ "v0" : 9, "v1" : 10, "trait" : "kickOffBarrier", "curve" : 180, "cGroup" : ["blueKO" ] },
+		{ "v0" : 9, "v1" : 10, "trait" : "kickOffBarrier", "curve" : -180, "cGroup" : ["redKO" ] },
+		{ "v0" : 10, "v1" : 11, "trait" : "kickOffBarrier" },
+
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 20, "v1" : 21 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 22, "v1" : 20 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 22, "v1" : 23 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 24, "v1" : 25, "curve" : -173.974424992 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 26, "v1" : 27 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 26, "v1" : 28, "curve" : 198.924644416 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 28, "v1" : 27, "curve" : 198.924644416 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 29, "v1" : 30 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 29, "v1" : 30, "curve" : 178.994836667 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 31, "v1" : 32 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 32, "v1" : 33 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 32, "v1" : 34 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 35, "v1" : 36, "curve" : 173.974424992 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 37, "v1" : 38, "curve" : -173.974424992 },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "v0" : 39, "v1" : 40, "curve" : 173.974424992 }
+
+	],
+
+	"goals" : [
+		{ "p0" : [-370,64 ], "p1" : [-370,-64 ], "team" : "red" },
+		{ "p0" : [370,64 ], "p1" : [370,-64 ], "team" : "blue" }
+
+	],
+
+	"discs" : [
+		{ "pos" : [-370,64 ], "trait" : "goalPost", "color" : "FFCCCC" },
+		{ "pos" : [-370,-64 ], "trait" : "goalPost", "color" : "FFCCCC" },
+		{ "pos" : [370,64 ], "trait" : "goalPost", "color" : "CCCCFF" },
+		{ "pos" : [370,-64 ], "trait" : "goalPost", "color" : "CCCCFF" },
+
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "pos" : [-62.5,182 ], "radius" : 2, "color" : "000000" },
+		{ "pos" : [-68.5,193 ], "cMask" : ["ball" ], "cGroup" : ["ball" ], "radius" : 2, "color" : "000000" },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "pos" : [-56.5,193 ], "radius" : 2, "color" : "000000" },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "pos" : [-18.5,187 ], "radius" : 2, "color" : "000000" },
+		{ "cMask" : ["ball" ], "cGroup" : ["ball" ], "pos" : [18.5,187 ], "radius" : 2, "color" : "000000" }
+
+	],
+
+	"planes" : [
+		{ "normal" : [0,1 ], "dist" : -170, "trait" : "ballArea" },
+		{ "normal" : [0,-1 ], "dist" : -170, "trait" : "ballArea" },
+
+		{ "normal" : [0,1 ], "dist" : -200, "bCoef" : 0.1 },
+		{ "normal" : [0,-1 ], "dist" : -200, "bCoef" : 0.1 },
+		{ "normal" : [1,0 ], "dist" : -420, "bCoef" : 0.1 },
+		{ "normal" : [-1,0 ], "dist" : -420, "bCoef" : 0.1 }
+
+	],
+
+	"traits" : {
+		"ballArea" : { "vis" : false, "bCoef" : 1, "cMask" : ["ball" ] },
+		"goalPost" : { "radius" : 8, "invMass" : 0, "bCoef" : 0.5 },
+		"goalNet" : { "vis" : true, "bCoef" : 0.1, "cMask" : ["ball" ] },
+		"kickOffBarrier" : { "vis" : false, "bCoef" : 0.1, "cGroup" : ["redKO","blueKO" ], "cMask" : ["red","blue" ] }
+
+	},
+
+	"ballPhysics" : {
+		"radius" : 9.2,
+		"bCoef" : 1,
+		"damping" : 0.991,
+		"invMass" : 1,
+		"color" : "EDED09"
+
+	},
+
+	"playerPhysics" : {
+		"kickStrength" : 8.5,
+		"invMass" : 1e+26,
+		"bCoef" : 0
+
+	}
+}
 room.pluginSpec = {
   name: `hr/simple-commands`,
   author: `salamini`,
   version: `1.1.1`,
-  dependencies: [`sav/commands`, `sav/players`],
+  dependencies: [`hr/commands`, `hr/players`],
 };
 
 room.onCommand_bb = {
@@ -24,7 +175,7 @@ room.onCommand_bb = {
     room.kickPlayer(player.id, "Good Bye!", false);
   },
   data: {
-    'sav/help': {
+    'hr/help': {
       text: ' (leave the room)',
     }
   }
@@ -40,6 +191,10 @@ room.onCommand_pm = {
       );
       return false;
     }
+room.onCommand_classic = {
+
+    room.setCustomStadium(strongClassic);
+}
 
     const mutePlugin = room.getPlugin("hr/mute");
     if (mutePlugin && mutePlugin.isMuted(fromPlayer)) {
@@ -91,7 +246,7 @@ room.onCommand_pm = {
     return false;
   },
   data: {
-    'sav/help': {
+    'hr/help': {
       text: ' #PLAYER_ID (send a private message to another player)',
     }
   }
@@ -110,7 +265,7 @@ room.onCommand_swap = {
     }
   },
   data: {
-    'sav/help': {
+    'hr/help': {
       text: ' (swap teams if you have admin)',
     }
   }
@@ -129,7 +284,7 @@ room.onCommand_rr = {
     }
   },
   data: {
-    'sav/help': {
+    'hr/help': {
       text: ' (restart game if you have admin)',
     }
   }
@@ -159,7 +314,7 @@ room.onCommand_rrs = {
     }
   },
   data: {
-    'sav/help': {
+    'hr/help': {
       text: ' (restart game and swap teams if you have admin)',
     }
   }
